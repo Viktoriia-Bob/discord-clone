342c5e31ae93609011c574300f31a8f590e5e722
Viktoriia Bob <vikabobir32@gmail.com>
2023-12-08 14:08:27 +0200

    feat: add delete and leave modal

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 import { Server } from "@prisma/client";
 
-export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel";
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer";
 
 interface ModalData {
     server?: Server;
